9e9c04ae68ebb0a636c73a08f647f93b30b922be
z50205 <z50205@yahoo.com.tw>
2025-06-19 00:17:04 +0800

    [Debug]Resolve testing error with mock data, and update CICD file.

--- PSS/test_app.py
+++ PSS/test_app.py
@@ -1,4 +1,5 @@
 from fastapi.testclient import TestClient
+from unittest.mock import patch, MagicMock
 
 from app import app
 
@@ -10,10 +11,16 @@ def test_post_login_with_empty_credentials():
     assert "Please enter both your username and password." in response.text
 
 def test_post_login_with_invalid_credentials():
-    response = client.post("/", data={"username": "123", "password": "wrong"})
-    assert "Invalid username or password." in response.text
+    fake_user = MagicMock()
+    fake_user.check_pw.return_value = False
+    with patch("models.UserData.query_name", return_value=fake_user):
+        response = client.post("/", data={"username": "123", "password": "wrong"})
+        assert "Invalid username or password." in response.text
 
 def test_get_index_with_session_redirect():
-    response = client.post("/", data={"username": "123", "password": "123123"},follow_redirects=False)
-    assert response.status_code == 303
-    assert response.headers["location"].endswith("/chooseroom")
+    fake_user = MagicMock()
+    fake_user.check_pw.return_value = True
+    with patch("models.UserData.query_name", return_value=fake_user):
+        response = client.post("/", data={"username": "123", "password": "123123"},follow_redirects=False)
+        assert response.status_code == 303
+        assert response.headers["location"].endswith("/chooseroom")
